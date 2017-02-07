be0b3b41cece426e4cef9b07ff36af8d81dd6229
nikhil <nikhil.v.more@gmail.com>
2017-02-07 08:17:07 +0530

    my first commit

--- src/main/java/jaxwsExample/ws/CodeTest.java
+++ src/main/java/jaxwsExample/ws/CodeTest.java
@@ -1,14 +1,32 @@
 package jaxwsExample.ws;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
 /**
  * 
  * @author Nikhil
- * 
- * service interface
+ *  service implemetation
  *
  */
-public interface CodeTest {
+@WebService
+@SOAPBinding(style = Style.RPC)
+public class CodeTest{
 
-	public String checkMyCode();
+	
+	public String checkMyCode(String name) {
+		
+		int count = 1;//fetch this value from database to store count of hits.  
+		count++;
+		System.out.println("timestamp:" + System.currentTimeMillis());
+		System.out.println("calls" + count);
+		return "{timestamp : " + System.currentTimeMillis()+" calls : " + count +"}";
+	}
+
 
 }
